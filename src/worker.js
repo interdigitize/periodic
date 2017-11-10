84e2d2c5164afcd66e2c2845cb0195053d59a216
interdigitize <interdigitize@users.noreply.github.com>
2017-11-10 12:01:14 -0800

    Adds symbols, atomic numbers, and atomic mass to elements and renders them in a row

--- src/worker.js
+++ src/worker.js
@@ -5,6 +5,7 @@ var DOMElement = require('famous/dom-renderables/DOMElement');
 var FamousEngine = require('famous/core/FamousEngine');
 var Position = require('famous/components/Position');
 var Align = require('famous/components/Align');
+var Size = require('famous/components/Size')
 
 // periodicTable npm module
 var periodicTable = require('periodic-table');
@@ -14,16 +15,32 @@ var allElements = periodicTable.all();
 var scene = FamousEngine.createScene('body');
 var rootNode = scene.addChild();
 
+var inALine = 0;
 // a function to create elements
-var elementMaker = function (elName) {
+var elementMaker = function (elName, elSymbol, elAtomicNum, elAtomicMass) {
   var node = rootNode.addChild();
-  node.setPosition((Math.random() * 1000), (Math.random() * 500));
-  return new DOMElement(node, {
-    content: elName
-  });
+  var nodeEl = new DOMElement(node);
+  nodeEl.setContent(`
+    <div class='node'>
+      <p class='atomicNum'>${elAtomicNum}</p>
+      <h1 class='symbol'>${elSymbol}</h1>
+      <p class='name'>${elName}</p>
+      <p class='atomicMass'>${elAtomicMass}</p>
+    </div>
+  `);
+
+  var nodeSize = new Size(node);
+  nodeSize.setMode('absolute', 'absolute').setAbsolute(100, 100);
+
+  node.setPosition(inALine, 0);
+  inALine += 110;
+  return node;
 }
 
-//create elements for each element in the periodic table
-var periodicElements = allElements.map( obj => {
-  return elementMaker(obj.name)
+var roundMass = function(num, index) {
+  return Math.round(parseFloat(num) * 1000) / 1000;
+}
+
+var periodicElements = allElements.map( (obj, index) => {
+  return elementMaker(obj.name, obj.symbol, obj.atomicNumber, roundMass(obj.atomicMass, index))
 })
